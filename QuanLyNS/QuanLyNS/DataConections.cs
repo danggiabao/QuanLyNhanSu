493cf357d1f59a0d007e1836286557f4545c888a
Kiên Lưu <kienlt543@gmail.com>
2018-04-23 14:55:58 +0700

    DataConnections

---
+++ QuanLyNS/QuanLyNS/DataConections.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyNS
+{
+	public class DataConections
+	{
+		string Strcon = @"Data Source=DESKTOP-2NN1U31\SQLEXPRESS;Initial Catalog=QL_GVHS;Integrated Security=True";
+ 		public SqlConnection conn = null;
+		public void OpenConnection()
+		{
+			if (conn == null) // kiểm tra có kết nối chưa..chưa kết nối thì sẽ khởi tạo kết nối
+				conn = new SqlConnection(Strcon);
+			if (conn.State == ConnectionState.Closed)
+				conn.Open();
+		}
+		public void CloseConnection()
+		{
+			if (conn != null && conn.State == ConnectionState.Open)
+				conn.Close();
+		}
+	}
+}
