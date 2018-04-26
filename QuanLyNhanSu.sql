0c0256d85da0d9b36dabb2c8f6e2590dcfb89328
Dang Bao <dangbaoxx@gmail.com>
2018-04-26 12:09:43 +0700

    update lai CSDL phân chia proc theo chú thích từng bảng

--- QuanLyNhanSu.sql
+++ QuanLyNhanSu.sql
@@ -56,6 +56,104 @@ CREATE TABLE DANGNHAP
 	MatKhau VARCHAR(30)
 )
 GO
+INSERT dbo.DANGNHAP( TaiKhoan, MatKhau )
+VALUES  ( N'admin','123456'),
+		( N'user1','123456')
+GO
+INSERT INTO dbo.PhongBan( MaPB, TenPB, DiaChi, SDT, MaTP )
+VALUES  ('PB01', N'Hành Chính',N'Hà Nội', '09032289456', 'NV01'),
+		('PB02', N'Tài Chính',N'Hà Nội', '016458956', 'NV02'),
+		('PB03', N'Quản lý', N'Hà Nội', '0163228946', 'NV03')
+GO
+INSERT INTO dbo.TRINHDOHOCVAN ( MaTDHV, TenTrinhDo, ChuyenNganh )
+VALUES  ('TD01',N'Cử nhân đại học',N'CNTT'),
+		('TD02',N'Kỹ sư đại học',N'CNTT'),
+		('TD03',N'Cử nhân đại học',N'Kinh Tế')
+GO
+insert into NhanVien(MaNV,HoTen,GioiTinh,SDT,QueQuan,NgaySinh,MaTDHV,MaPB)
+values	('NV01',N'Lê Vân Anh',N'Nữ','0916564789',N'Hà Nội','01-08-1984','TD01','PB01')
+insert into NhanVien(MaNV,HoTen,GioiTinh,SDT,QueQuan,NgaySinh,MaTDHV,MaPB)
+Values		('NV02',N'Trịnh Quang Vinh',N'Nam','0916564719',N'Nam Định','10-12-1982','TD03','PB02')
+insert into NhanVien(MaNV,HoTen,GioiTinh,SDT,QueQuan,NgaySinh,MaTDHV,MaPB)
+Values		('NV03',N'Đỗ Văn Nam',N'Nam','0916564889',N'Hà Tây','06-05-1990','TD02','PB03')
+insert into NhanVien(MaNV,HoTen,GioiTinh,SDT,QueQuan,NgaySinh,MaTDHV,MaPB)
+Values		('NV04',N'Nguyễn Trung Kiên',N'Nam','0916560389',N'Hà Nội','11-08-1992','TD02','PB01')
+insert into NhanVien(MaNV,HoTen,GioiTinh,SDT,QueQuan,NgaySinh,MaTDHV,MaPB)
+Values		('NV05',N'Nguyễn Thị Ngọc Ánh',N'Nữ','01647386289',N'Phú Thọ','02-09-1989','TD01','PB01')
+insert into NhanVien(MaNV,HoTen,GioiTinh,SDT,QueQuan,NgaySinh,MaTDHV,MaPB)
+Values		('NV06',N'Trần Lâm',N'Nam','0912564781',N'Hà Nội','11-08-1995','TD03','PB02')
+GO
+
+INSERT INTO dbo.CHUCVU( MaCV, TenCV )
+VALUES  ('CV01',N'Giám Đốc'),
+		('CV02',N'Trường Phòng'),
+		('CV03',N'Phó Giám Đốc'),
+		('CV04',N'Phó Phòng'),
+		('CV05',N'Trường Nhóm'),
+		('CV06',N'Phó Nhóm'),
+		('CV07',N'Nhân Viên'),
+		('CV08',N'Thư Ký')
+GO
+INSERT dbo.NHANVIEN_CHUCVU(MaNV, MaCV)
+VALUES  ('NV01','CV01'),
+		('NV02','CV02'),
+		('NV03','CV03'),
+		('NV04','CV04'),
+		('NV05','CV05'),
+		('NV06','CV06')
+GO
+
+INSERT INTO dbo.HOPDONGLAODONG(MaHD,LoaiHD,LuongThoaThuan,MaNV)
+VALUES	('HD01',N'Chính Thức',20000000,'NV01'),
+		('HD02',N'Chính Thức',15000000,'NV02'),
+		('HD03',N'Chính Thức',13000000,'NV03'),
+		('HD04',N'Chính Thức',10000000,'NV04'),
+		('HD05',N'Chính Thức',10000000,'NV05'),
+		('HD06',N'Chính Thức',10000000,'NV06')
+GO
+--Proc Dang Nhap
+CREATE PROC DangKy(@Taikhoan NVARCHAR(30),@Matkhau NVARCHAR(30))
+AS
+	BEGIN
+	INSERT dbo.DANGNHAP( TaiKhoan, MatKhau )
+	VALUES  ( @Taikhoan, @Matkhau )
+END
+GO
+
+CREATE PROC dbo.KiemTraDN(@Username VARCHAR(50),@Pass varchar(50)) AS 
+BEGIN
+	SELECT * FROM dbo.DANGNHAP WHERE TaiKhoan = @Username AND MatKhau = @Pass
+END
+GO
+
+--Proc Chuc Vu
+CREATE PROC THEMCV (@machucvu VARCHAR(10),@tenchucvu NVARCHAR(50))
+AS
+BEGIN
+	INSERT dbo.CHUCVU( MaCV, TenCV)
+	VALUES  ( @machucvu,@tenchucvu)
+	
+END
+
+GO
+CREATE PROC SUACV (@machucvu VARCHAR(10),@tenchucvu NVARCHAR(50))
+AS
+BEGIN
+	UPDATE dbo.CHUCVU
+	SET TenCV=@tenchucvu
+	WHERE MaCV=@machucvu
+END
+
+GO 
+CREATE PROC XOACV (@machucvu varchar(10))
+AS
+BEGIN
+		DELETE dbo.CHUCVU WHERE MaCV=@machucvu
+END
+GO
+--
+
+--Proc Phong Ban
 CREATE PROC ThemPB(@MaPB VARCHAR(10),@TenPB NVARCHAR(50),@MaTP VARCHAR(10),@DiaChi NVARCHAR(50),@SDT CHAR(15)) AS
 BEGIN
 	 INSERT INTO PHONGBAN
@@ -75,6 +173,8 @@ CREATE PROC XoaPB(@MaPB VARCHAR(10)) AS
 		DELETE FROM PHONGBAN WHERE MaPB=@MaPB
  END
 go
+--
+--Proc Hop Dong
 CREATE PROC ThemHD(@MaHD VARCHAR(10),@LoaiHD NVARCHAR(30),@Luongthoathuan BIGINT,@MaNV VARCHAR(10)) AS
 BEGIN
 	 INSERT INTO HOPDONGLAODONG
@@ -93,6 +193,6 @@ CREATE PROC XoaHD(@MaHD VARCHAR(10)) AS
  BEGIN
 		DELETE FROM HOPDONGLAODONG WHERE MaHD=@MaHD
  END
-
+ ---
 
  
